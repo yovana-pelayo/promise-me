95a45f63216459ea44f03101061987a7f4ac8a70
Yovana Pelayo <94511266+yovana-pelayo@users.noreply.github.com>
2022-04-26 09:07:02 -0700

    Initial commit

---
+++ src/services/promise-me.js
@@ -0,0 +1,32 @@
+/**
+ * TODO: Exercise 1: use `fetch` & async/await to get data from an API
+ */
+
+/**
+ * TODO: Exercise 2: use `fetch` & `.then` syntax to get the same data from the first exercise.
+ */
+
+/**
+ * TODO: Exercise 3: use `fetch`, async/await, and a try/catch statement to get data from an API and handle errors
+ */
+
+/**
+ * TODO: Exercise 4: use `fetch`, `.then`, and `.catch` to get the same data from exercise 3 while handling errors
+ */
+
+/**
+ * TODO: Exercise 5: use `fetch`, async/await, and a try/catch/finally statement to get data from an API, handle errors, then console.log('All done!') upon completion
+ */
+
+/**
+ * TODO: Exercise 6: use `fetch`, `.then`, `.catch`. and `.finally` to get the same data from exercise 5 while handling errors and calling console.log('All done!') upon completion
+ */
+
+/**
+ * TODO: Exercise 7: Call your function from exercise 7 using .then
+ */
+
+/**
+ * TODO: Exercise 8: Call your function from exercise 6 using async/await
+ */
+
